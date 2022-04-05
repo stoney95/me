2d8e214817a23e1d0cbe69d93c090780edf72211
SimonS <simon.steinheber@maibornwolff.de>
2022-04-05 18:25:01 +0200

    Setup repo

---
+++ blueprint-templates/react-component/__pascalCase_name__/__pascalCase_name__-container.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {default as {{pascalCase name}}View} from "./{{pascalCase name}}-view";
+
+function {{pascalCase name}}Container() {
+    return (<{{pascalCase name}}View />)
+}
+
+export default {{pascalCase name}}Container;
---
+++ blueprint-templates/react-component/__pascalCase_name__/__pascalCase_name__-view.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./{{pascalCase name}}.scss";
+
+function {{pascalCase name}}View(props) => {return (<></>)}
+
+export default {{pascalCase name}}View;
