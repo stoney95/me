ec0679c625cc96d40cfde837156cb1359c561e20
SimonS <simon.steinheber@maibornwolff.de>
2022-04-05 21:02:06 +0200

    Add header

---
+++ src/components/Header/Header-container.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import {default as HeaderView} from "./Header-view";
+
+function HeaderContainer() {
+    return (<HeaderView />)
+}
+
+export default HeaderContainer;
---
+++ src/components/Header/Header-view.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./Header.scss";
+
+function HeaderView() {
+    return <header>
+        <div className="text-box">
+            <span>This is me</span>
+        </div>
+    </header>
+}
+
+export default HeaderView;
