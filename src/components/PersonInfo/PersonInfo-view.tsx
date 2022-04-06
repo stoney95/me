da473b2e9e7ffdd9f344978a80aafa8347ae0829
SimonS <simon.steinheber@maibornwolff.de>
2022-04-06 17:48:53 +0200

    Finish person info

--- src/components/PersonInfo/PersonInfo-view.tsx
+++ src/components/PersonInfo/PersonInfo-view.tsx
@@ -2,30 +2,53 @@ import React from "react";
 
 import "./PersonInfo.scss";
 import pic from "../../assets/profile-picture.jpg"
+import {details, shortDescription} from "./data"
 
-function Detail(icon: string, text: string) {
+
+
+type DetailProps = {
+    icon: string;
+    text: string | Element;
+}
+
+
+function Detail({icon, text}: DetailProps) {
     return (
         <div className="detail">
-            <img src={icon}/>
+            <img className="icon" src={icon}/>
             <div className="detail-text">{text}</div>
+        </div>
+    )
+}
+
+function DetailView(details: Array<DetailProps>) {
+    return (
+        <div className="d-flex flex-row justify-content-center flex-wrap">
+            {details.map(({icon, text}) => <Detail icon={icon} text={text} />)}
         </div>
     )
 }
 
 function PersonInfoView() {
     return (
-        <div className="person-info">
-            <div>
-                <div className="outer-border">
-                    <div className="inner-border">   
-                        <img src={pic}/>
+        <div className="d-flex flex-column">
+            <div className="d-flex flex-row align-items-center person-info">
+                <div>
+                    <div className="outer-border">
+                        <div className="inner-border">   
+                            <img className="profile-picture" src={pic}/>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className="details-box">
-                <div className="name-box">
-                    <span>Simon Steinheber</span>
+                <div className="details-box">
+                    <div className="name-box">
+                        <span>Simon Steinheber</span>
+                    </div>
+                    {DetailView(details)}
                 </div>
+            </div>
+            <div className="short-description">
+                {shortDescription}
             </div>
         </div>
     )
---
+++ src/components/PersonInfo/data.ts
@@ -0,0 +1,25 @@
+import location_pic from "./../../assets/location.png"
+import mobile_pic from "./../../assets/mobile.png"
+import mail_pic from "./../../assets/mail.png"
+import github_pic from "./../../assets/github.png"
+
+export const details = [
+    {
+        icon: location_pic,
+        text: "Munich"
+    },
+    {
+        icon: mobile_pic,
+        text: "+49 15758910134"
+    },
+    {
+        icon: mail_pic,
+        text: "simon@steinheber.info"
+    },
+    {
+        icon: github_pic,
+        text: "stoney95"
+    },
+]
+
+export const shortDescription = "Data generalist ❤️ python, data, projects, automation  🤓 glad to learn something new every day  🔨 building productive ML systems and end-to-end data products"
